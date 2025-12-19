a2fcd0193b1a03b4048287e8de1017abbd90ae0d
Pablo Irusta <pirusta@fsa.com>
2025-12-19 16:41:14 -0300

    primer cambio

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ import './DATOS_PASTELERIA.JS';
 import './FUNCIONES.JS';
 import './ALIAS.js';
 import './DATOS_LISTADOS.JS';
+import { recetas } from './DATOS_PASTELERIA.JS';
 
 
 const categorias = document.getElementById("categorias");
 categorias.addEventListener("change", function() {
     const seleccion = categorias.value;
     console.log("Categoría seleccionada:", seleccion);
+    if (seleccion === "Pasteleria") {
+        recetas.forEach(receta => { 
+            console.log("Receta:", receta);
+        });
+    }
     // Aquí puedes agregar la lógica para filtrar las recetas según la categoría seleccionada
 });
