74f61e86f54e94fb85950f3f34ee33cde36878c0
Carlos <carlosrizzetti@gmail.com>
2026-01-15 21:23:59 -0300

    commit de la rama

--- Logica/pro.js
+++ Logica/pro.js
@@ -1,8 +1,5 @@
 import { Receta } from "../Logica/recetas.js";
-/* const btnCategorias = document.querySelectorAll("button");
-btnCategorias.forEach(btn => {
-  btn.addEventListener("click", () => irARecetas(btn.textContent));
-}); */
+
 
 const receta = new Receta();
 receta.render();
