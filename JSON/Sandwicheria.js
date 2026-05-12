b043e1e961e93cdca091e04c5423219af6e0128f
Pablo Irusta <pirusta@fsa.com>
2026-05-12 13:24:36 -0300

    Arreglos 12/5

--- JSON/Sandwicheria.js
+++ JSON/Sandwicheria.js
@@ -104,5 +104,35 @@ export const recetasSandwiches = [
       { nombre: "Queso", bruto: 0.035, limpio: 0, cocido: 0 },
       { nombre: "El papel Sulfito Individual", bruto: 1.000, limpio: 0, cocido: 0 }
     ]
+  },
+  {
+    Mercaderia: "Mc cheese chicken Termosellado (MELI)",
+    Ingredientes: [
+      { nombre: "Pan bimbo alargado", bruto: 1, limpio: 0, cocido: 0 },
+      { nombre: "Medallón de pollo", bruto: 2, limpio: 0, cocido: 0 },
+      { nombre: "Queso cheddar feteado", bruto: 0.064, limpio: 0, cocido: 0 },
+      
+      
+    ]
+  },
+  {
+    Mercaderia: "Cheese burger Termosellado (Meli)",
+    Ingredientes: [
+      { nombre: "Pan bimbo con sesamo", bruto: 1, limpio: 0, cocido: 0 },
+      { nombre: "Queso chddar feteado", bruto: 0.064, limpio: 0, cocido: 0 },
+      { nombre: "Hamburguesa de 69 grs", bruto: 2, limpio: 0, cocido: 0 },
+      
+    ]
+  },
+  {
+    Mercaderia: "Ciabatta de milanesa de ternera Termosellado (MELI)",
+    Ingredientes: [
+      { nombre: "Pan ciabatta", bruto: 1, limpio: 0, cocido: 0 },
+      { nombre: "Milanesa de ternera", bruto: 1, limpio: 0, cocido: 0 },
+      { nombre: "Jamon cocido", bruto: 0.020, limpio: 0, cocido: 0 },
+      { nombre: "Queso de barra", bruto:0.035, limpio: 0, cocido: 0 },
+      
+      
+    ]
   }
 ];
