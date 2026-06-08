594b95339435e436c5dfc26a9bc6c4e67b2f424a
Carlosrizzetti <pirusta@fsa.com>
2026-06-08 15:20:43 -0300

    7/6

--- JSON/Sandwicheria.js
+++ JSON/Sandwicheria.js
@@ -108,14 +108,12 @@ export const recetasSandwiches = [
   {
     Mercaderia: "Sandw. Burger Mc ALVAREZ ",
     Ingredientes: [      
-      { nombre: "AJO", bruto: 0.010, limpio: 0, cocido: 0 },
+      
       { nombre: "BARBACOA", bruto: 0.028, limpio: 0, cocido: 0 },
       { nombre: "CEBOLLA", bruto: 0.058, limpio: 0, cocido: 0 },
       { nombre: "CHORIZO COLORADO", bruto: 0.003, limpio: 0, cocido: 0 },
-      { nombre: "HAMBURGUESA 69 GRS", bruto: 2.060, limpio: 0, cocido: 0 },
-      { nombre: "MAIZENA", bruto: 0.001, limpio: 0, cocido: 0 },
-      { nombre: "MIEL", bruto: 0.001, limpio: 0, cocido: 0 },
-      { nombre: "PAN PARA HAMBURGUESAS BIMBO", bruto: 1.03, limpio: 0, cocido: 0 },
+      { nombre: "HAMBURGUESA 69 GRS", bruto: 2, limpio: 0, cocido: 0 },      
+      { nombre: "PAN PARA HAMBURGUESAS BIMBO", bruto: 1, limpio: 0, cocido: 0 },
       { nombre: "PANCETA AHUMADA", bruto: 0.003, limpio: 0, cocido: 0 },      
       { nombre: "QUESO CHEDDAR FETEADO", bruto: 0.024, limpio: 0, cocido: 0 },
       { nombre: "REPOLLO BLANCO", bruto: 0.031, limpio: 0, cocido: 0 },
